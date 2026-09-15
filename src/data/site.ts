660f5c872185ee838447744605a3a58bed4ab042
IsActive <tha.smoovie@gmail.com>
2026-09-15 21:02:47 +0200

    Update contact email and href in site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -26,8 +26,8 @@ export const PERSON = {
 export const CONTACT = {
   phone: "+48 518 474 719",
   phoneHref: "tel:+48518474719",
-  email: "bozena.siminiak@gmail.com",
-  emailHref: "mailto:bozena.siminiak@gmail.com",
+  email: "psychologia.blizejsiebie@gmail.com",
+  emailHref: "psychologia.blizejsiebie@gmail.com",
 };
 
 export const OBSZARY = [
